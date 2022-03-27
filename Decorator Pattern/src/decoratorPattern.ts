68fae5392b50d07b718cc1484c2d9fba07825b43
christophermlee2 <christophermlee2@gmail.com>
2022-03-27 18:19:02 -0400

    updated decorator

---
+++ Decorator Pattern/src/decoratorPattern.ts
@@ -0,0 +1,5 @@
+let coins = 100
+
+const addDamageButton = document.querySelector(".upgrade-weapon.tab")!;
+const addModifierButton = document.querySelector(".upgrade-mod.tab")!;
+const addSpecialAttributeButton = document.querySelector(".upgrade-special.tab")!;
