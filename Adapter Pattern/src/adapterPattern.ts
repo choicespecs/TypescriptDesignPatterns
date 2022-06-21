1d00b9e423151d7a2ed02f1b09500219fccfde70
christophermlee2 <christophermlee2@gmail.com>
2022-06-21 01:42:40 -0400

    updated adapter

--- Adapter Pattern/src/adapterPattern.ts
+++ Adapter Pattern/src/adapterPattern.ts
@@ -3,118 +3,131 @@ Example One:
 Interface 1: Media Player (play)
 Interface 2: Advanced Media Player (playVid, playMusic)
 */
+
+// two target interfaces which implement different methods for media players
 interface MediaPlayer {
-    play(mediaType : string, fileName : string) : void;
+  play(mediaType: string, fileName: string): void;
 }
 
 interface AdvancedMediaPlayer {
-    playVid(fileName: string) : void;
-    playMusic(fileName: string) : void;
+  playVid(fileName: string): void;
+  playMusic(fileName: string): void;
 }
 
 class YouTubePlayer implements AdvancedMediaPlayer {
-    playVid(fileName : string) {
-        console.log(`opened youtube video: ${fileName}`);
-    }
+  playVid(fileName: string) {
+    console.log(`opened youtube video: ${fileName}`);
+  }
 
-    playMusic(fileName: string): void {}
+  playMusic(fileName: string): void {}
 }
 
 class Mp4Player implements AdvancedMediaPlayer {
-    playVid(fileName: string): void {}
+  playVid(fileName: string): void {}
 
-    playMusic(fileName: string): void {
-        console.log(`Play Music Player`);
-    }
+  playMusic(fileName: string): void {
+    console.log(`Play Music Player`);
+  }
 }
 
 class MediaAdapter implements MediaPlayer {
+  private advancedMediaPlayer: AdvancedMediaPlayer;
 
-    private advancedMediaPlayer : AdvancedMediaPlayer;
-
-    constructor(mediaType: string) {
-        if (mediaType === "youtube") {
-            this.advancedMediaPlayer = new YouTubePlayer();
-        } else if (mediaType === "mp4") {
-            this.advancedMediaPlayer = new Mp4Player();
-        }
+  constructor(mediaType: string) {
+    if (mediaType === "youtube") {
+      this.advancedMediaPlayer = new YouTubePlayer();
+    } else if (mediaType === "mp4") {
+      this.advancedMediaPlayer = new Mp4Player();
     }
+  }
 
-    play(mediaType : string, fileName : string) {
-        if (mediaType === "youtube") {
-            this.advancedMediaPlayer.playVid(fileName);
-        } else if (mediaType === "mp4") {
-            this.advancedMediaPlayer.playMusic(fileName);
-        }
+  play(mediaType: string, fileName: string) {
+    if (mediaType === "youtube") {
+      this.advancedMediaPlayer.playVid(fileName);
+    } else if (mediaType === "mp4") {
+      this.advancedMediaPlayer.playMusic(fileName);
     }
+  }
 }
 
+/*
+    we have an adaptee that is meant to interact with the target & subclases
+    we use the adapter so we can interface with target that may not match
+    perfectly with our class. 
+ */
 class AudioTextReader implements MediaPlayer {
-    private mediaAdapter : MediaAdapter;
+  private mediaAdapter: MediaAdapter;
 
-    play(audioType: string, fileName: string) {
-        if (audioType === "txt") {
-            console.log(`Reading txt ${fileName}`)
-        } else {
-            this.mediaAdapter = new MediaAdapter(audioType);
-            this.mediaAdapter.play(audioType, fileName);
-        }
+  play(audioType: string, fileName: string) {
+    if (audioType === "txt") {
+      console.log(`Reading txt ${fileName}`);
+    } else {
+      this.mediaAdapter = new MediaAdapter(audioType);
+      this.mediaAdapter.play(audioType, fileName);
     }
+  }
 }
 
 /*
 Example Two:
 */
 
-// Target
+// Target which a client may only see target...
 interface CreditCardPayment {
-    getBankDetails() : void;
-    getCreditCard(): string;
+  getBankDetails(): void;
+  getCreditCard(): string;
 }
 
-// Adaptee
+/* 
+    Adaptee which we want to connect together with the target
+    but we do not have any way currently to connect the two together
+    without adding additional implementations or code
+*/
 class BankDetails {
-    private bankName : string;
-    private accName : string;
-    private accNumber : number;
+  private bankName: string;
+  private accName: string;
+  private accNumber: number;
 
-    getBankName() {
-        return this.bankName;
-    }
+  getBankName() {
+    return this.bankName;
+  }
 
-    setBankName(bankName : string) {
-        this.bankName = bankName;
-    }
+  setBankName(bankName: string) {
+    this.bankName = bankName;
+  }
 
-    getAccName() {
-        return this.accName;
-    }
+  getAccName() {
+    return this.accName;
+  }
 
-    setAccName(accName : string) {
-        this.accName = accName;
-    }
+  setAccName(accName: string) {
+    this.accName = accName;
+  }
 
-    getAccNumber() {
-        return this.accNumber;
-    }
+  getAccNumber() {
+    return this.accNumber;
+  }
 
-    setAccNumber(accNumber : number) {
-        this.accNumber = accNumber;
-    }
+  setAccNumber(accNumber: number) {
+    this.accNumber = accNumber;
+  }
 }
 
-// Adapter class
+/* Adapter class
+ allows adaptee to interact together with the target to
+*/
 class BankCustomer extends BankDetails implements CreditCardPayment {
+  getBankDetails(): void {
+    const accNumber = this.getAccNumber();
+    const accName = this.getAccName();
+    const bankName = this.getBankName();
+    console.log(
+      `Account Number: ${accNumber}, Account Name; ${accName}, Bank Name; ${bankName}`
+    );
+  }
 
-    getBankDetails(): void {
-        const accNumber = this.getAccNumber();
-        const accName = this.getAccName();
-        const bankName = this.getBankName();
-        console.log(`Account Number: ${accNumber}, Account Name; ${accName}, Bank Name; ${bankName}`);
-    }
-
-    getCreditCard(): string {
-        const accNumber = this.getAccNumber();
-        return accNumber.toString();
-    }
+  getCreditCard(): string {
+    const accNumber = this.getAccNumber();
+    return accNumber.toString();
+  }
 }
